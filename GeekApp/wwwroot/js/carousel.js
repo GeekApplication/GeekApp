04b8f36629a83f812d6ce726b2f1d372bb9c1c09
Ismailabrahem <143405336+Ismailabrahem@users.noreply.github.com>
2025-05-21 03:01:08 +0200

    Final changes made added responsiveness to all pages fixed homepage with slideshow carousel fixed navbar for responsiveness new mapping for homepage added cookie consent

---
+++ GeekApp/wwwroot/js/carousel.js
@@ -0,0 +1,46 @@
+﻿// wwwroot/js/carousel.js
+let currentIndex = 0;
+let items = [];
+let timer;
+
+function initCarousel() {
+    items = document.querySelectorAll('.carousel-item');
+    if (items.length > 0) {
+        items.forEach((item, index) => {
+            item.style.display = index === 0 ? 'block' : 'none';
+        });
+        startAutoRotation();
+    }
+}
+
+function startAutoRotation() {
+    timer = setInterval(() => {
+        currentIndex = (currentIndex + 1) % items.length;
+        updateCarousel();
+    }, 5000);
+}
+
+function updateCarousel() {
+    items.forEach((item, index) => {
+        item.style.display = index === currentIndex ? 'block' : 'none';
+    });
+}
+
+function nextItem() {
+    clearInterval(timer);
+    currentIndex = (currentIndex + 1) % items.length;
+    updateCarousel();
+    startAutoRotation();
+}
+
+function prevItem() {
+    clearInterval(timer);
+    currentIndex = (currentIndex - 1 + items.length) % items.length;
+    updateCarousel();
+    startAutoRotation();
+}
+
+// Make functions available globally
+window.initCarousel = initCarousel;
+window.nextItem = nextItem;
+window.prevItem = prevItem;
--- GeekApp/wwwroot/js/scroll.js
+++
@@ -1,7 +0,0 @@
-﻿//window.getScrollPosition = function () {
-//    return window.scrollY || window.pageYOffset;
-//};
-
-//window.setScrollPosition = function (position) {
-//    window.scrollTo(0, position);
-//};
